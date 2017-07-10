cfb14b77fe20c39e9b94ede37775753eb96e31fc
Mariano D'Agostino <mariano@tictapps.com>
2017-07-10 20:56:16 -0300

    Relacion

---
+++ Colors.h
@@ -0,0 +1,15 @@
+//
+//  Colors.h
+//  Project
+//
+//  Created by Mariano D'Agostino on 7/10/17.
+//  Copyright © 2017 Morena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Colors : NSObject
+
+    - (UIColor*) mainColor;
+    
+@end
---
+++ Colors.m
@@ -0,0 +1,15 @@
+//
+//  Colors.m
+//  Project
+//
+//  Created by Mariano D'Agostino on 7/10/17.
+//  Copyright © 2017 Morena. All rights reserved.
+//
+
+#import "Colors.h"
+
+@implementation Colors
+    - (UIColor*) mainColor {
+        return [UIColor greenColor];
+    }
+@end
